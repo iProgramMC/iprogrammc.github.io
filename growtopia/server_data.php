b9a63ba31644cc507ea888c401c8100841e00552
iProgramInCpp <andrei.cluci.ac@gmail.com>
2019-11-18 21:02:46 +0200

    Update server_data.php

--- growtopia/server_data.php
+++ growtopia/server_data.php
@@ -1,10 +1,10 @@
-server|127.0.0.1
-port|17091
+server|5.231.205.163
+port|25566
 type|1
 maint|Mainetrance message (Not used for now) -- Growtopia Noobs
 
-beta_server|127.0.0.1
-beta_port|17091
+beta_server|5.231.205.163
+beta_port|25566
 
 beta_type|1
 meta|localhost
