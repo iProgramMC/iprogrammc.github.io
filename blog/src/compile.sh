ab5cdbd2fc09d59cafb8bd7b0203ba869399896f
iProgramInCpp <iprogramincpp@gmail.com>
2024-11-25 22:35:57 +0200

    * Add metadata, please work.

--- blog/src/compile.sh
+++ blog/src/compile.sh
@@ -28,8 +28,8 @@ for Article in $(ls -t $ArticlesDir/*); do
 	echo "Compiling article $ArticleBase"
 	
 	# Grep for the title, while skipping the TITLE: marker
-	ArticleTitle=$(grep 'TITLE:' $Article)
-	ArticleTitle=$(echo $ArticleTitle | cut -b 7-)
+	ArticleTitle=$(grep 'TITLE:' $Article | tr -d '\n' | cut -b 7-)
+	ArticleDescr=$(grep 'DESCRIPTION:' $Article)
 	
 	# Stat the article for the last mod time, then convert it to a string using date
 	ArticleLastModUnix=$(stat -c '%Y' $Article)
@@ -37,10 +37,10 @@ for Article in $(ls -t $ArticlesDir/*); do
 	ArticleLastMod2=$(date -d @$ArticleLastModUnix +'%Y-%m-%d at %T')
 	
 	# Print out the header while modifying the title and the last modification date.
-	cat $HeaderFile | sed "s/XXX_TITLE_HERE_XXX/$ArticleTitle/g;s/XXX_LAST_MOD_DATE_XXX/$ArticleLastMod/g" > $ArticleDest
+	cat $HeaderFile | sed "s/XXX_TITLE_HERE_XXX/$ArticleTitle/g;s/XXX_LAST_MOD_DATE_XXX/$ArticleLastMod/g;s/XXX_DESCRIPTION_HERE_XXX/$ArticleDescr/g;s/XXX_POST_NAME_XXX/$ArticleBase/g" > $ArticleDest
 	
 	# Remove the TITLE: from the markdown, and then convert it to html and concatenate it into ArticleDest
-	grep -v 'TITLE:' $Article | $MarkdownPl >> $ArticleDest
+	grep -v 'TITLE:' $Article | grep -v 'DESCRIPTION:' | $MarkdownPl >> $ArticleDest
 	
 	# Finally, concatenate the footer file into $ArticleDest.
 	cat $FooterFile | sed "s/XXX_TITLE_HERE_XXX/$ArticleTitle/g;s/XXX_LAST_MOD_DATE_XXX/$ArticleLastMod/g" >> $ArticleDest
